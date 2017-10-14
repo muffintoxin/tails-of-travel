35318a5517a49c6fd2c2c78925e085eccf3c2aac
Adelya Fatykhova <afatykhova@gmail.com>
2017-10-14 20:48:38 +0200

    Testing heroku: changed server listen port

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!')
-})
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
 
 app.use(express.static('./public'))
+
