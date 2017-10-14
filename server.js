b925e545aaa4272c41f652cc27624e03df66a801
Adelya Fatykhova <afatykhova@gmail.com>
2017-10-14 20:51:19 +0200

    Testing heroku: changed port from 80 to 3000

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
