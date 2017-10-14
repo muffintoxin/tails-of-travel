03314f89a433aa161f08e1b7cb482cb1098efc49
Adelya Fatykhova <afatykhova@gmail.com>
2017-10-14 20:19:41 +0200

    Changed console log line on app start

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('App listening on port 3000!')
 })
 
 app.use(express.static('./public'))
