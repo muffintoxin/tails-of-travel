6739bb4976819824a247c80272c1d92e9b8c900e
Adelya Fatykhova <afatykhova@gmail.com>
2017-10-16 21:15:29 +0200

    Modified server file to publish slidedeck

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/',function(req,res){
   res.sendFile('/index.html');
 });
 
+app.get('/concept',function(req,res){
+  res.sendFile('/public/docs/Concept_Slidedeck_2017.pdf');
+});
+
 var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 app.listen(server_port, server_host, function() {
